e63907afc53cba51484b9b572e95762ebe3f5379
Kaung Thant Zin <86509255+KaungThantZin2021@users.noreply.github.com>
2024-09-23 22:06:34 +0630

    [Fix] layout

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,111 +1,59 @@
 <template>
     <div>
-        <nav class="fixed top-10 left-20 right-20">
-            <div class="mx-auto max-w-7xl">
-                <div class="relative flex h-16 items-center justify-between">
-                    <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                        <!-- Mobile menu button-->
-                        <button type="button"
-                            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                            aria-controls="mobile-menu" aria-expanded="false">
-                            <span class="absolute -inset-0.5"></span>
-                            <span class="sr-only">Open main menu</span>
-                            <!--
-            Icon when menu is closed.
-
-            Menu open: "hidden", Menu closed: "block"
-          -->
-                            <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                            </svg>
-                            <!--
-            Icon when menu is open.
-
-            Menu open: "block", Menu closed: "hidden"
-          -->
-                            <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                            </svg>
-                        </button>
-                    </div>
-                    <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                        <div class="flex flex-shrink-0 items-center">
-                            <NuxtLink 
-                                to="/"          
-                                class="font-medium tracking-widest transition ease-in-out duration-300 hover:scale-125"
-                            >
-                                Portfolio
-                            </NuxtLink>
-                        </div>
-                        
-                    </div>
-                    <div
-                        class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                        <div class="hidden sm:ml-6 sm:block">
-                            <div class="flex space-x-10">
-
-                                <NuxtLink 
-                                    to="/"          
-                                    :class="{
-                                        'text-sm font-medium scale-125 transition ease-in-out duration-300 hover:scale-150': route.path === '/',
-                                        'text-sm font-medium transition ease-in-out duration-300 hover:scale-150': route.path !== '/'
-                                    }"
-                                >
-                                    Home
-                                </NuxtLink>
-
-                                <NuxtLink 
-                                    to="/skills" 
-                                    :class="{
-                                        'text-sm font-medium scale-125 transition ease-in-out duration-300 hover:scale-150': route.path === '/skills',
-                                        'text-sm font-medium transition ease-in-out duration-300 hover:scale-150': route.path !== '/skills'
-                                    }"
-                                >
-                                    Skills
-                                </NuxtLink>
-                                
-
-                                <NuxtLink 
-                                    to="/about" 
-                                    :class="{
-                                        'text-sm font-medium scale-125 transition ease-in-out duration-300 hover:scale-150': route.path === '/about',
-                                        'text-sm font-medium transition ease-in-out duration-300 hover:scale-150': route.path !== '/about'
-                                    }"
-                                >
-                                    About
-                                </NuxtLink>
-
-                                <NuxtLink 
-                                    to="/contact" 
-                                    :class="{
-                                        'text-sm font-medium scale-125 transition ease-in-out duration-300 hover:scale-150': route.path === '/contact',
-                                        'text-sm font-medium transition ease-in-out duration-300 hover:scale-150': route.path !== '/contact'
-                                    }"
-                                >
-                                    Contact
-                                </NuxtLink>
-
-                            </div>
-                        </div>
-                    </div>
+        <nav class="fixed top-10 left-20 right-20 flex h-16 items-center justify-between">
+            <div class="flex flex-1 items-center justify-start">
+                <div class="flex flex-shrink-0 items-center">
+                    <NuxtLink 
+                        to="/"          
+                        class="font-semibold tracking-widest transition ease-in-out duration-300 hover:scale-125"
+                    >
+                        Portfolio
+                    </NuxtLink>
                 </div>
             </div>
+            <div class="flex items-center pr-2">
+                <div class="flex space-x-10">
+                    <NuxtLink 
+                        to="/"          
+                        :class="{
+                            'text-sm font-medium scale-125 transition ease-in-out duration-300 hover:scale-150': route.path === '/',
+                            'text-sm font-medium transition ease-in-out duration-300 hover:scale-150': route.path !== '/'
+                        }"
+                    >
+                        Home
+                    </NuxtLink>
 
-            <!-- Mobile menu, show/hide based on menu state. -->
-            <div class="sm:hidden" id="mobile-menu">
-                <div class="space-y-1 px-2 pb-3 pt-2">
-                    <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                    <a href="#" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
-                        aria-current="page">Dashboard</a>
-                    <a href="#"
-                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
-                    <a href="#"
-                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
-                    <a href="#"
-                        class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
+                    <NuxtLink 
+                        to="/skills" 
+                        :class="{
+                            'text-sm font-medium scale-125 transition ease-in-out duration-300 hover:scale-150': route.path === '/skills',
+                            'text-sm font-medium transition ease-in-out duration-300 hover:scale-150': route.path !== '/skills'
+                        }"
+                    >
+                        Skills
+                    </NuxtLink>
+                    
+
+                    <NuxtLink 
+                        to="/about" 
+                        :class="{
+                            'text-sm font-medium scale-125 transition ease-in-out duration-300 hover:scale-150': route.path === '/about',
+                            'text-sm font-medium transition ease-in-out duration-300 hover:scale-150': route.path !== '/about'
+                        }"
+                    >
+                        About
+                    </NuxtLink>
+
+                    <NuxtLink 
+                        to="/contact" 
+                        :class="{
+                            'text-sm font-medium scale-125 transition ease-in-out duration-300 hover:scale-150': route.path === '/contact',
+                            'text-sm font-medium transition ease-in-out duration-300 hover:scale-150': route.path !== '/contact'
+                        }"
+                    >
+                        Contact
+                    </NuxtLink>
+
                 </div>
             </div>
         </nav>
